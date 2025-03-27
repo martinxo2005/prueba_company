d7f9fdf07a5f1781c170d6df1005189b728b93a4
Martin <martindelarosan@gmail.com>
2025-03-27 18:39:10 +0100

    primera version

---
+++ src/main/java/es/soterohernandez/PROYECTO_db_company/App.java
@@ -0,0 +1,10 @@
+package es.soterohernandez.PROYECTO_db_company;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/es/soterohernandez/PROYECTO_db_company/AppTest.java
@@ -0,0 +1,19 @@
+package es.soterohernandez.PROYECTO_db_company;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
